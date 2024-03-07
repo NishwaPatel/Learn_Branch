8c92d2eeb789d4fa3c4b9c199b7659504b035c9b
NishwaPatel <nishwapatel3@gmail.com>
2024-03-07 14:40:20 +0530

    added more lines

--- nish.py
+++ nish.py
@@ -1,3 +1,8 @@
 '''hello there this is nish and i am writing here a paragraph..'''
 
 '''i am uploading the images here...'''
+
+
+'''this is more lines you talked about'''
+'''1)good morning'''
+'''2)good afternoon'''
